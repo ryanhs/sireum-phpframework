5c66ff894c38b4e17a1ca43e5401a59fcf3d4d09
Ryan H. S. <mr.ryansilalahi@gmail.com>
2012-07-24 12:53:39 +0700

    version 2.4

--- example/4/index.php
+++ example/4/index.php
@@ -1,2 +1,8 @@
 <?php
 require '../../sireum.php';
+
+
+$sireum->add('index', function() use($sireum){
+	$sireum->form->addRule('test', 'test', 'required', $method = 'get');
+	var_dump($sireum->form->validation(), $sireum->form->getError());
+});
